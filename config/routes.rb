2b76d9f6adfcb827766c200b24128c5567c68e99
daudi13 <davouma13@gmail.com>
2022-07-13 08:53:06 +0300

    readjust path to static

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,10 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  namespace :api do
+  namespace :api , default: {format: 'json'} do
     resources :greetings, only: [:index]
   end
-  
-    root "greetings#index"
+
+    root "static#index"
+    get '*path', to: 'static#index'
 end
