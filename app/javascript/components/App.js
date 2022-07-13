16bfc971a6573029106d3733cd2d6f2731cb3955
daudi13 <davouma13@gmail.com>
2022-07-13 10:52:58 +0300

    modify App.js file

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,25 +1,23 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
-
-import { Provider } from "react-redux"
-
-import configureStore from "../configureStore"
-const store = configureStore()
-
-import HelloWorld from "./HelloWorld"
+import React from 'react';
+import PropTypes from 'prop-types';
+import Greeting from './Greeting';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from '../store/configureStore';
 class App extends React.Component {
-  render () {
+  render() {
     return (
-      <Provider store={store}>
-      <Router>
-        <Switch>
-          <Route path="/" render={() => <HelloWorld greeting="Friend"/>} />
-        </Switch>
-      </Router>
-      </Provider>
+      <React.StrictMode>
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Greeting />} />
+            </Routes>
+          </Router>
+        </Provider>
+      </React.StrictMode>
     );
   }
 }
 
-export default App
+export default App;
